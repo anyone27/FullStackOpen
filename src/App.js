c2bd1d83175bbc5070096c5ef818e08b263c9713
anyone27 <josh@joshcallarman.com>
2022-02-03 15:27:58 +0000

    click counter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React, { useState } from 'react'
-
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ onClick, text }) =>
-  <button onClick={onClick}>
-    {text}
-  </button>
+import React, {
+    useState
+} from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const Reset = () => setCounter(0)
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'));
+        setLeft(left + 1);
+    }
 
-  return (
-    <div>
-    <Display counter={counter} />
-    <Button onClick={increaseByOne} text="Plus 1"/>
-    <Button onClick={decreaseByOne} text="Minus 1"/>
-    <Button onClick={Reset} text="Reset"/>
-    </div>
-  )
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'));
+        setRight(right + 1);
+    }
+
+    return (
+        <div> {left}
+            <button onClick={handleLeftClick}>left</button>
+            <button onClick={handleRightClick}>right</button>
+            {right}
+            <p>{
+                allClicks.join(' ')
+            }</p>
+        </div>
+    )
 }
 
 export default App
